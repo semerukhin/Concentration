dd023b2d919532e56684817da7a6bf78bae8cd87
Ilya Semerukhin <semerukhin@gmail.com>
2018-05-06 16:00:09 +0300

    Fix code indentation

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -9,65 +9,66 @@
 import Foundation
 
 class Concentration {
-	
-	private(set) var cards = [Card]()
-	
-	private var indexOfOneAndOnlyFaceUpCard: Int? {
-		get {
-			return cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly
-			
-//			let faceUpCardIndices = cards.indices.filter({ cards[$0].isFaceUp })
-//			return faceUpCardIndices.count == 1 ? faceUpCardIndices.first : nil
-			
-//			var foundIndex: Int?
-//			for index in cards.indices {
-//				if cards[index].isFaceUp {
-//					if foundIndex == nil {
-//						foundIndex = index
-//					} else {
-//						return nil
-//					}
-//				}
-//			}
-//			return foundIndex
-		}
-		set {
-			for index in cards.indices {
-				cards[index].isFaceUp = (index == newValue)
-			}
-		}
-	}
-	
-	func chooseCard(at index: Int) {
-		assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index): chosen index is not in the cards")
-		if !cards[index].isMatched {
-			if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-				if cards[matchIndex] == cards[index] {
-					cards[matchIndex].isMatched = true
-					cards[index].isMatched = true
-				}
-				cards[index].isFaceUp = true
-			} else {
-				indexOfOneAndOnlyFaceUpCard = index
-			}
-		}
-	}
-	
-	init(numberOfPairOfCards: Int) {
-		assert(numberOfPairOfCards > 0, "Concentration.init(\(numberOfPairOfCards)): you must have at least one pair of cards")
-		for _ in 0..<numberOfPairOfCards {
-			let card = Card()
-			cards += [card, card]
-		}
-		// TODO: Shuffle the cards
-	}
-
+   
+   private(set) var cards = [Card]()
+   
+   private var indexOfOneAndOnlyFaceUpCard: Int? {
+      get {
+         return cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly
+         
+//         let faceUpCardIndices = cards.indices.filter({ cards[$0].isFaceUp })
+//         return faceUpCardIndices.count == 1 ? faceUpCardIndices.first : nil
+         
+//         var foundIndex: Int?
+//         for index in cards.indices {
+//            if cards[index].isFaceUp {
+//               if foundIndex == nil {
+//                  foundIndex = index
+//               } else {
+//                  return nil
+//               }
+//            }
+//         }
+//         return foundIndex
+         
+      }
+      set {
+         for index in cards.indices {
+            cards[index].isFaceUp = (index == newValue)
+         }
+      }
+   }
+   
+   func chooseCard(at index: Int) {
+      assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index): chosen index is not in the cards")
+      if !cards[index].isMatched {
+         if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+            if cards[matchIndex] == cards[index] {
+               cards[matchIndex].isMatched = true
+               cards[index].isMatched = true
+            }
+            cards[index].isFaceUp = true
+         } else {
+            indexOfOneAndOnlyFaceUpCard = index
+         }
+      }
+   }
+   
+   init(numberOfPairOfCards: Int) {
+      assert(numberOfPairOfCards > 0, "Concentration.init(\(numberOfPairOfCards)): you must have at least one pair of cards")
+      for _ in 0..<numberOfPairOfCards {
+         let card = Card()
+         cards += [card, card]
+      }
+      // TODO: Shuffle the cards
+   }
+   
 }
 
 extension Collection {
-	var oneAndOnly: Element? {
-		return count == 1 ? first : nil
-	}
+   var oneAndOnly: Element? {
+      return count == 1 ? first : nil
+   }
 }
 
 
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,69 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-	
-	private lazy var game = Concentration(numberOfPairOfCards: (cardButtons.count + 1) / 2)
-	private(set) var flipCount = 0 { didSet { updateFlipCountLabel() } }
-	private var emojiChoices = "🎃👻🍭🦇🍎😈😱"
-	private var emoji = [Card: String]()
-	
-	@IBOutlet private weak var flipCountLabel: UILabel! { didSet { updateFlipCountLabel() } }
-	@IBOutlet private var cardButtons: [UIButton]!
-	
-	@IBAction private func touchCard(_ sender: UIButton) {
-		flipCount += 1
-		if let cardNumber = cardButtons.index(of: sender) {
-			game.chooseCard(at: cardNumber)
-			updateViewFromModel()
-		} else {
-			print("nil!")
-		}
-	}
-	
-	private func updateViewFromModel() {
-		for index in cardButtons.indices {
-			let button = cardButtons[index]
-			let card = game.cards[index]
-			if card.isFaceUp {
-				button.setTitle(emoji(for: card), for: UIControlState.normal)
-				button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-			} else {
-				button.setTitle("", for: UIControlState.normal)
-				button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
-			}
-		}
-	}
-	
-	private func emoji(for card: Card) -> String {
-		if emoji[card] == nil {
-			let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
-			emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
-		}
-		return emoji[card] ?? "?"
-	}
-	
-	private func updateFlipCountLabel() {
-		// flipCountLabel.text = "Flips: \(flipCount)"
-		let attributes: [NSAttributedStringKey: Any] = [
-			.strokeWidth: 5.0,
-			.strokeColor: #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
-		]
-		let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
-		flipCountLabel.attributedText = attributedString
-	}
-	
+   
+   private lazy var game = Concentration(numberOfPairOfCards: (cardButtons.count + 1) / 2)
+   private(set) var flipCount = 0 { didSet { updateFlipCountLabel() } }
+   private var emojiChoices = "🎃👻🍭🦇🍎😈😱"
+   private var emoji = [Card: String]()
+   
+   @IBOutlet private weak var flipCountLabel: UILabel! { didSet { updateFlipCountLabel() } }
+   @IBOutlet private var cardButtons: [UIButton]!
+   
+   @IBAction private func touchCard(_ sender: UIButton) {
+      flipCount += 1
+      if let cardNumber = cardButtons.index(of: sender) {
+         game.chooseCard(at: cardNumber)
+         updateViewFromModel()
+      } else {
+         print("nil!")
+      }
+   }
+   
+   private func updateViewFromModel() {
+      for index in cardButtons.indices {
+         let button = cardButtons[index]
+         let card = game.cards[index]
+         if card.isFaceUp {
+            button.setTitle(emoji(for: card), for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+         } else {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+         }
+      }
+   }
+   
+   private func emoji(for card: Card) -> String {
+      if emoji[card] == nil {
+         let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+         emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
+      }
+      return emoji[card] ?? "?"
+   }
+   
+   private func updateFlipCountLabel() {
+      // flipCountLabel.text = "Flips: \(flipCount)"
+      let attributes: [NSAttributedStringKey: Any] = [
+         .strokeWidth: 5.0,
+         .strokeColor: #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+      ]
+      let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+      flipCountLabel.attributedText = attributedString
+   }
+   
 }
 
 extension Int {
-	var arc4random: Int {
-		if self == 0 {
-			return 0
-		} else if self > 0 {
-			return Int(arc4random_uniform(UInt32(self)))
-		} else {
-			return -Int(arc4random_uniform(UInt32(abs(self))))
-		}
-	}
+   var arc4random: Int {
+      if self == 0 {
+         return 0
+      } else if self > 0 {
+         return Int(arc4random_uniform(UInt32(self)))
+      } else {
+         return -Int(arc4random_uniform(UInt32(abs(self))))
+      }
+   }
 }
 
 

36b0a3f3e6a832eb67bdcdbf896d2f7d3c80bfeb
Ilya Semerukhin <semerukhin@gmail.com>
2018-05-13 20:50:02 +0300

    Change theme on the fly by conditional segue

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -17,14 +17,32 @@ class ConcentrationThemeChooserViewController: UIViewController {
    ]
    
    @IBAction func changeTheme(_ sender: Any) {
-      performSegue(withIdentifier: "Choose Theme", sender: sender)
+      if let cvc = splitViewDetailConcentrationViewController {
+         if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+            cvc.theme = theme
+         }
+      } else if let cvc = lastSeguedToConcentrationViewController {
+         if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+            cvc.theme = theme
+         }
+         navigationController?.pushViewController(cvc, animated: true)
+      } else {
+         performSegue(withIdentifier: "Choose Theme", sender: sender)
+      }
    }
+   
+   private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+      return splitViewController?.viewControllers.last as? ConcentrationViewController
+   }
+   
+   private var lastSeguedToConcentrationViewController: ConcentrationViewController?
    
    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
       if segue.identifier == "Choose Theme" {
          if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
             if let cvc = segue.destination as? ConcentrationViewController {
                cvc.theme = theme
+               lastSeguedToConcentrationViewController = cvc
             }
          }
       }

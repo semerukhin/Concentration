42b5f842019c48a72cb4ea647f50da6b9f4b0874
Ilya Semerukhin <semerukhin@gmail.com>
2018-05-13 21:19:20 +0300

    Show themes chooser before game

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ConcentrationThemeChooserViewController: UIViewController {
+class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
    
    let themes = [
       "Sports":  "⚽️🏀🏈⚾️🎾🏐🏉🎱🥌🏓",
       "Faces":   "😀😄😅☺️😉😘🙁😢🤔😬",
       "Animals": "🐶🐭🐹🦊🐼🐷🦁🐵🐨🐻"
    ]
+   
+   override func awakeFromNib() {
+      splitViewController?.delegate = self
+   }
+   
+   func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+      if let cvc = secondaryViewController as? ConcentrationViewController {
+         if cvc.theme == nil {
+            return true
+         }
+      }
+      return false
+   }
    
    @IBAction func changeTheme(_ sender: Any) {
       if let cvc = splitViewDetailConcentrationViewController {
